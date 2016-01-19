59ab61b128531f18e8bc360eaff60c9d559e3d8b
Mitchell Hwang <mdhwang@mit.edu>
2016-01-19 15:08:42 -0500

    added c++ files from first lecture

--- c++/inheritance.cpp
+++ c++/inheritance.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+
 using namespace std;
 
 class Animal{
@@ -7,4 +10,31 @@ class Animal{
         Animal(string s){
             name = s;
         }
-        ~
+        
+        ~Animal() {
+            cout << "bye, " << name << endl;
+        }
+
+        virtual string noise() = 0;
+
+        void speak() {
+            cout << noise() << endl;
+        }
+};
+
+class Cow : public Animal {
+    protected:
+        int milk;
+    public:
+        Cow(string s) : Animal(s) { milk = 10; }
+        string noise() override { return "mooo"; } // c++11
+};
+
+class OddCow : public Cow {
+    private:
+        int sprite;
+    public:
+        OddCow(string s) : Cow(s) { this->sprite = this->milk; }
+        string noise() { return "neigh"; }
+};
+

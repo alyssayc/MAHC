a62fd18f5b5b6b30cabf9c4ff100ede4577ea7ea
alyssayc@mit.edu <alyssayc@mit.edu>
2016-01-23 13:25:14 -0500

    working hangman

--- hangman.cpp
+++ hangman.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include "color.h" // include colors
+#include <typeinfo>
 
 using namespace std;
 
@@ -100,7 +101,7 @@ void clearScreen(){
 }
 
 
-void printGuess(vector<char> guess){
+int printGuess(vector<char> guess){
 
 	cout << "Current Status: "; 
 	int count=0;
@@ -111,57 +112,142 @@ void printGuess(vector<char> guess){
 		cout << BOLDCYAN << guess[i] << " " << RESET;
 	}
 	cout << endl << "You have " << count << " letters left to guess" << endl << endl;
+	return count;
 }
+
 
 
 int main() {
 
-	int strikes = 0;
+	int strikes = 0, playAgain = 0;
 	string phrase, tmp;
+	char tryAgain;
 
 	//Player enters word for other player to guess
 	//Once they confirm their word, player hands over to other player to guess
-	cout << "Hello! Let's play hangman!" << endl << BOLDCYAN << "Enter your phrase to guess: " << RESET ;
-	getline(cin, phrase);
+	while (playAgain == 0){
+		cout << "Hello! Let's play hangman!" << endl << BOLDCYAN << "Enter your word to guess: " << RESET ;
+		cin >> phrase;
 
-	cout << "Your phrase is : " << phrase << ". If that's correct, type 'ready' to clear the screen, then hand the game over to the guesser:  ";
-	cin >> tmp;
+		cout << "Your word is : " << BOLDCYAN << phrase << RESET << ". If that's correct, type 'ready' to clear the screen, then hand the game over to the guesser:  ";
+		cin >> tmp;
 
-	if (tmp.compare("ready") == 0){
-		clearScreen();
-	}
+		int ready=0;
+		while (ready==0){
+			if (tmp.compare("ready") == 0){
+				ready = 1;
+				clearScreen();
+			}
+			else {
+				cout << endl << endl << "Oops. Looks like you're not ready! Type 'ready' when you're ready to clear the screen, then hand the game over to the guesser: ";
+				cin >> tmp;
+			}
 
-
-	//Other Player Begins:
-	vector<char> guess, tmp2;
-
-	for (int i = 0; i < phrase.length() ; i++){
-		guess.push_back('*');
-	}
-	
-	displayMan(strikes);
-	printGuess(guess);
-	
-	//Time to Guess!
-	//ask to enter letter or guess the word
-	char letter;
-	cout << "Enter your letter: ";
-	cin >> letter;
-	cout << endl << "letter is " << letter << endl;
-
-	for (int i = 0; i < phrase.length() ; i++){
-		cout << "Letter is: " << letter << " and actual is " << phrase[i] << endl;
-		if(letter == phrase[i]){
-			tmp2.push_back(phrase[i]);
 		}
-		else{
-			tmp2.push_back('*');
+		
+
+
+		//Other Player Begins:
+		vector<char> guess, recentChanges, compare;
+
+		//instantiates guess to be "* * * * *.." to match the num of chars of the phrase
+		for (int i = 0; i < phrase.length() ; i++){
+			guess.push_back('*');
 		}
+		
+		//initial display of man and current status
+		displayMan(strikes);
+		printGuess(guess);
+		
+		//Time to Guess!
+		//ask to enter letter or guess the word
+		char letter;
+		int boo = 0; //if boo is 0, that means the guessed letter is not in the phrase --> increment strikes
+		//int isLetter = 0; //0 if the letter entered is not a char, if it is isLetter = 1
+		while (strikes < 8){
+
+			//while (isLetter == 0) {
+				cout << "Enter your letter: ";
+				cin >> letter;
+				cout << typeid(letter).name() << endl;
+				/*if (typeid(letter).name() == 'c'){
+					isLetter = 1;
+				}*/
+			//}
+			cout << "======================================================================";
+			//cout << endl << "letter is " << letter << endl;
+
+			for (int i = 0; i < phrase.length() ; i++){
+			//cout << "Letter is: " << letter << " and actual is " << phrase[i] << endl;
+				if(letter == phrase[i]){
+					recentChanges.push_back(phrase[i]);
+					boo = 1;
+				}
+				else{
+					recentChanges.push_back('*');
+				}
+			}
+
+			//increment strikes if didn't hit a match
+			if (boo == 0){
+				strikes++; 
+			}
+
+			//update guess to reflect recent changes
+			// guess = current guess with past changes
+			// recentChanges = vector with recent (if any) changes
+			if (boo == 1){
+				for (int i = 0; i < guess.size(); i++){
+					if (guess[i] != '*'){
+						compare.push_back(guess[i]);
+					}
+					else if (recentChanges[i] != '*'){
+						compare.push_back(recentChanges[i]);
+					}
+					else{
+						compare.push_back('*');
+					}
+				}
+
+				guess=compare;
+			}
+
+
+			compare.clear();
+			recentChanges.clear();
+			boo=0;
+			//cout << "Strikes: " << strikes << endl;
+			
+			displayMan(strikes);
+			//printGuess(guess);
+
+
+			if (strikes == 7){
+				
+				cout << "Uh oh. You lost. Try again? (y/n): ";
+				cin >> tryAgain;
+				if (tryAgain == 'n'){
+					cout << "Thanks for playing!" << endl;
+					playAgain = 1;
+				}
+				break;
+			}
+
+			if (printGuess(guess) == 0){
+				cout << "You guessed the word!! Play again? (y/n): ";
+				cin >> tryAgain;
+				if (tryAgain == 'n'){
+					cout << "Thanks for playing!" << endl;
+					playAgain = 1;
+				}
+				break;
+			} 
+		}
+
+		
 	}
 
-	guess=tmp2;
-
-	printGuess(guess);
+	
 	//testing comparing chars
 
 	/*phrase = "hi";

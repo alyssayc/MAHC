3a0b31ad25dc3054d7a08a2ba716bed8046d1b82
Mitchell Hwang <mdhwang@mit.edu>
2016-01-24 12:34:55 -0500

    Compiled after fixing main, needs work on display

--- tttu.cpp
+++ tttu.cpp
@@ -15,37 +15,55 @@ _____|_____|_____ | _____|_____|_____ | _____|_____|_____
      |     |      |      |     |      |      |     |     
      |     |      |      |     |      |      |     |     
 ---------------------------------------------------------
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-_____|_____|_____ | _____|_____|_____ | _____|_____|_____
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-_____|_____|_____ | _____|_____|_____ | _____|_____|_____
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
+     |     |      |      |     |      |    __________
+     |     |      |      |     |      |   /          \
+_____|_____|_____ | _____|_____|_____ |  /            \ 
+     |     |      |      |     |      |  |            | 
+     |     |      |      |     |      |  |            | 
+_____|_____|_____ | _____|_____|_____ |  |            |
+     |     |      |      |     |      |  \            /
+     |     |      |      |     |      |   \__________/
+     |     |      |      |     |      | 
 ---------------------------------------------------------
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-_____|_____|_____ | _____|_____|_____ | _____|_____|_____
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-_____|_____|_____ | _____|_____|_____ | _____|_____|_____
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
+     |     |      |      |     |      |     \       /
+     |     |      |      |     |      |      \     /
+_____|_____|_____ | _____|_____|_____ |       \   /
+     |     |      |      |     |      |        \ / 
+     |     |      |      |     |      |         x 
+_____|_____|_____ | _____|_____|_____ |        / \
+     |     |      |      |     |      |       /   \
+     |     |      |      |     |      |      /     \
+     |     |      |      |     |      |     /       \
+
+Okay instead of printing the entire board
+Ill print the small version of big board
+And then Ill print whatever small board they are marking in
+and I will give them an image of any small board if they want.
+
+     |     |      
+     |     |     
+_____|_____|_____
+     |     |     
+     |     |     
+_____|_____|_____
+     |     |      
+     |     |      
+     |     |      
+
+     |     |      
+     |     |     
+_____|_____|_____
+     |     |     
+     |     |     
+_____|_____|_____
+     |     |      
+     |     |      
+     |     |      
+
 */
 
-// Not converted
-bool gameOver(vector<char> board){ // Returns whether the game is over or not.
-	// Should be a 3x3 board in this instance
-	/*
-	if (strlen(board) != 9){
-		cout << strlen(board) << endl;
-		cout << "Sorry, but I do believe that this is not a standard tic-tac-toe board size! " << endl;
-		return true; // should exit the loop
-	} */
-	// Check win conditions vertically and horizontally
+
+bool gameOverSmallBoard(vector<char> board){ // Returns whether the game is over or not.
 	for (int i = 0; i < 3; i++){
 		if ((board[3 * i] == board[3 * i + 1] && board[3 * i + 1] == board[3 * i + 2]) 
 			&& (board[3 * i] != *" " && board[3 * i + 1] != *" " && board[3 * i + 2] != *" ")){
@@ -63,8 +81,57 @@ bool gameOver(vector<char> board){ // Returns whether the game is over or not.
 	return false;
 }
 
+bool gameOverBigBoard(vector< vector<char> > board, vector<char> X_win, vector<char> O_win){
+	for (int i = 0; i < 3; i++){
+		if ((board[3 * i] == board[3 * i + 1] && board[3 * i + 1] == board[3 * i + 2]) 
+			&& (board[3 * i] == X_win || board[3 * i] == O_win)){
+			return true; // If the condition is true, then there is a row that has been completed
+		} else if ((board[i] == board[i + 3] && board[i + 3] == board[i + 6])
+				&& (board[i] == X_win || board[i] == O_win)){
+			return true;
+		} 
+	}
+	// Now check the diagonals
+	if (
+		(
+		(board[0] == X_win || board[0] == O_win) 
+		&& (board[0] == board[4] && board[4] == board[8])
+		) 
+		|| 
+		(
+		(board[2] == X_win || board[2] == O_win) 
+		&& (board[2] == board[4] && board[4] == board[6])
+		)
+		)
+	{
+		return true;
+	}
+	return false;
+}
+/*
+bool gameOverBigBoard(vector< vector<char> > board){
+	for (int i = 0; i < 3; i++){
+		if ((board[3 * i] == board[3 * i + 1] && board[3 * i + 1] == board[3 * i + 2]) 
+			&& (board[3 * i] != *" " && board[3 * i + 1] != *" " && board[3 * i + 2] != *" ")){
+			return true; // If the condition is true, then there is a row that has been completed
+		} else if ((board[i] == board[i + 3] && board[i + 3] == board[i + 6])
+				&& (board[i] != *" " && board[i + 3] != *" " && board[i + 6] != *" ")){
+			return true;
+		} 
+	}
+	// Now check the diagonals
+	if (((board[0] != *" " && board[4] != *" " && board[8] != *" ") && (board[0] == board[4] && board[4] == board[8])) 
+		|| ((board[2] != *" " && board[4] != *" " && board[6] != *" ") && (board[2] == board[4] && board[4] == board[6]))){
+		return true;
+	}
+	return false;
+}*/
+
 // Not converted
+/*
 void displayBoard(vector< vector<char> > board){ // Change this to look better and because this is a template.... 
+	// First display the first row...
+
 	cout << "     |     |     " << endl;
 	cout << "  " << board[0] << "  |  " << board[1] << "  |  " << board[2] << endl;
 
@@ -81,9 +148,10 @@ void displayBoard(vector< vector<char> > board){ // Change this to look better a
 	cout << "     |     |     " << endl << endl;
 	// Displays the current board... 
 	// Can implement color maybe....
-}
+}*/
 
 // finished converting
+
 bool checkSpace(vector<char> board, int index){ // returns whether the space is open or not
 	if ((board[index] == *"X") || (board[index] == *"O")){
 		return false;
@@ -92,62 +160,126 @@ bool checkSpace(vector<char> board, int index){ // returns whether the space is 
 	}
 }
 
+
 int main(){
-	
-	/*
-	cout << "Hello! Would you like to play tic-tac-toe? (y/n) ";
-	string answer;
-	cin >> answer;
-	int tick = 0;
-	// can implement a series of dialogue, an option to go back to games home?
-	
-	while (answer != "y"){
-		if (answer == "n"){
-			tick++;
-			if (tick < 6){
-				cout << "Oh... " << endl << "Would you like to play tic-tac-toe now? (y/n) ";
-			}
-		}
-		else {
-			cout << "Please enter in the format (y/n)!" << endl;
-		}
-		cin >> answer;
-	}
-    */
-
-
+	// instantiate the small tic tac toe boards;
 	vector<char> t_0(9); vector<char> t_1(9); vector<char> t_2(9); 
 	vector<char> t_3(9); vector<char> t_4(9); vector<char> t_5(9); 
 	vector<char> t_6(9); vector<char> t_7(9); vector<char> t_8(9);
+	// Create small tic tac toe boards for when any of them are won with X or O
+	vector<char> X_won(9); vector<char> O_won(9);
+
+	// Create the big tic tac toe board
 	vector< vector<char> > bigBoard(9, vector<char>(9));
+
+	// Set the values of big tic tac toe board
 	bigBoard[0] = t_0; bigBoard[1] = t_1; bigBoard[2] = t_2;
 	bigBoard[3] = t_3; bigBoard[4] = t_4; bigBoard[5] = t_5;
 	bigBoard[6] = t_6; bigBoard[7] = t_7; bigBoard[8] = t_8;
-	int index;
+
+	// instantiate the values in the small vectors to *" "
+	for (int i = 0; i < 9; i++){
+		for (int j = 0; j < 9; j++){
+			bigBoard[i][j] = *" ";
+		}
+	}
+
+	//Welcome
+	//Initialize values, enter names
+	int bigBoardIndex, smBoardIndex;
 	string Player1, Player2, temp;
-	cout << "Hello there! Welcome to Ultimate Tic Tac Toe!!!!!" << endl << "Player 1, please enter a name: ";
+	cout << "Player 1, please enter a name: ";
 	cin >> Player1;
 	cout << "Player 2, please enter a name: ";
 	cin >> Player2;
-	cout << endl << "Currently, it is" << Player1 << "'s turn. " << endl << "Please choose the number of the small tic-tac-toe board you would like make a move in." << endl;
-	int stttIndex; // Small Tic Tac Toe Index
-	cin >> stttIndex; // corresponds to which board in bigBoard will be altered
+	int counter = 0; // Keeps track of which player's turn it is.
+
+	// Begin with selecting indices and marking
+
+	// DISPLAY
+	cout << endl << "It is now " << Player1 << "'s turn." << endl;
+	cout << "Please choose from the big board, which small board to mark in." << endl;
+	// DISPLAY
+	cout << "Small Board #";
+	cin >> bigBoardIndex;
+	// Something about are you sure, you can go back, blah blah
+	cout << endl << "Please choose where in Small Board #" << bigBoardIndex << " to mark." << endl;
+	cin >> smBoardIndex;
+	bigBoard[bigBoardIndex][smBoardIndex] = *"X";
+	bigBoardIndex = smBoardIndex;
+	do{
+		// DISPLAY
+		temp = ((counter % 2) == 0) ? Player1 : Player2;
+		cout << "It is " << temp << "'s turn. " << endl;
+		
+		/*
+		while (gameOverSmallBoard(bigBoard[bigBoardIndex], bigBoardIndex)){
+		*/
+		if (bigBoardIndex == -1){
+			cout << "Small Board #" << bigBoardIndex << " has been won, please choose another small board." << endl;
+			cin >> bigBoardIndex;
+		}
+		// DISPLAY
+		cout << "You are currently viewing Small Board #" << bigBoardIndex << endl;
+		//cout << "Would you like to view another small board? (y/n)" ;
+		//DISPLAY
+		cout << "Please choose where in Small Board #" << bigBoardIndex << " to mark." <<endl;
+		cin >> smBoardIndex;
+		while (!checkSpace(bigBoard[bigBoardIndex], smBoardIndex)){
+			cout << "Sorry, that spot is occupied! Please choose another location: ";
+			cin >> smBoardIndex;
+		}
+		bigBoard[bigBoardIndex][smBoardIndex] = ((counter % 2) == 0) ? *"X" : *"O";
+		if (gameOverSmallBoard(bigBoard[bigBoardIndex])){
+			bigBoard[bigBoardIndex] = ((counter % 2) == 0) ? X_won : O_won;
+			bigBoardIndex = -1; // to reset the index
+		} else{
+			bigBoardIndex = smBoardIndex;
+		}
+		counter++;
+	} while(!gameOverBigBoard(bigBoard, X_won, O_won));
+
+	/*
+	int index;
+	string Player1, Player2, temp;
+	cout << "Hello there! Welcome to Ultimate Tic Tac Toe!!!!!" << endl                     << "Player 1, please enter a name: ";
+	cin >> Player1;
+	cout << "Player 2, please enter a name: ";
+	cin >> Player2;
+	cout << endl << "Currently, it is" << Player1 << "'s turn. " << endl;
+	int stttIndex = -1; // Small Tic Tac Toe Index
+	// corresponds to which board in bigBoard will be altered
 	// maybe add on stuff here like if same name, blah blah would you like a name? blah blah change name keep name blah blah
 	int counter = 0; // token to keep track of who's turn it is
 	do{
-		temp = ((counter % 2) == 0) ? Player1 : Player2;
-		cout << "It is " << temp << "'s turn. " << endl;
-		cout << "You are marking in small tic tac toe board #" << stttIndex << endl;
-		cout << "Please choose the # of the square in this stttb that you want to mark: ";
-		cin >> index;
-		if (!checkSpace(bigBoard[stttIndex], index)){
-			do{
-				cout << "Sorry, that spot is occupied! Please choose another location: ";
-				cin >> index;
-			} while (!checkSpace(bigBoard[stttIndex], index));
+		if (stttIndex == -1 || gameOverSmallBoard(bigBoard[stttIndex])){ // checks if a small board is finished
+			cout << "Please choose which small tic tac toe board to mark it." << endl;
+			cin >> stttIndex;
+			cout << "Please choose where in the small tic tac toe board to mark in." << endl;
+			cin >> index;
+		} else{
+			// display board
+			temp = ((counter % 2) == 0) ? Player1 : Player2;
+			cout << "It is " << temp << "'s turn. " << endl;
+			cout << "Please choose where in the small tic tac toe board to mark in." << endl;
+			cin >> index;
 		}
-
-	} while (false);
+		while (!checkSpace(bigBoard[stttIndex], index)){
+			cout << "Sorry, that spot is occupied! Please choose another location: ";
+			cin index;
+		}
+		bigBoard[stttIndex][index] = ((counter % 2) == 0) ? *"X" : *"O";
+		counter++;
+		stttIndex = index;
+	} while(!gameOverBigBoard(bigBoard));
+	// display board
+	if (gameOverBigBoard(bigBoard)){
+		cout << "Congratulations! " << temp << " has won!" << endl;
+	} else{
+		cout << "It's a tie!" << endl;
+	}
+	*/
+	
 }
 
 

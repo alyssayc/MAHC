d0b156ebef76779b202e1307fca009e17eebccb5
Mitchell Hwang <mdhwang@mit.edu>
2016-01-31 23:39:37 -0500

    Highlighted the current small tic tac toe board in tttu.cpp

--- tttu.cpp
+++ tttu.cpp
@@ -204,6 +204,191 @@ void displaySmallBoard(vector<char> board, int index){ // Change this to look be
 
 void displayBoard(vector< vector<char> > board, int bigBoardIndex){
 	cout << "Displaying the board." << endl;
+	cout << "     |     |      ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << "   ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << "   ";
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
+
+	cout << "_____|_____|_____ ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____ ";
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____" << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[0][3] << "  |  " << board[0][4] << "  |  " << board[0][5] << "   ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[1][3] << "  |  " << board[1][4] << "  |  " << board[1][5] << "   ";
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[2][3] << "  |  " << board[2][4] << "  |  " << board[2][5] << endl;
+
+	cout << "_____|_____|_____ ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____ ";
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____" << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[0][6] << "  |  " << board[0][7] << "  |  " << board[0][7] << "   ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[1][6] << "  |  " << board[1][7] << "  |  " << board[1][8] << "   ";
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[2][6] << "  |  " << board[2][7] << "  |  " << board[2][8] << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 0 || bigBoardIndex == 1){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 1 || bigBoardIndex == 2){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+
+	if (bigBoardIndex == 0 || bigBoardIndex == 3){cout << BOLDYELLOW;}
+	cout << "------------------" << BOLDBLUE;
+	if (bigBoardIndex == 0 || bigBoardIndex == 3 || bigBoardIndex == 1 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "-" << BOLDBLUE;
+	if (bigBoardIndex == 1 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "-------------------" << BOLDBLUE;
+	if (bigBoardIndex == 1 || bigBoardIndex == 4 || bigBoardIndex == 2 || bigBoardIndex == 5){cout << BOLDYELLOW;}
+	cout << "-" << BOLDBLUE;
+	if (bigBoardIndex == 2 || bigBoardIndex == 5){cout << BOLDYELLOW;}
+	cout << "------------------" << BOLDBLUE << endl;
+//
+	cout << "     |     |      ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[3][0] << "  |  " << board[3][1] << "  |  " << board[3][2] << "   ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[4][0] << "  |  " << board[4][1] << "  |  " << board[4][2] << "   ";
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[5][0] << "  |  " << board[5][1] << "  |  " << board[5][2] << endl;
+
+	cout << "_____|_____|_____ ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____ ";
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____" << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[3][3] << "  |  " << board[3][4] << "  |  " << board[3][5] << "   ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[4][3] << "  |  " << board[4][4] << "  |  " << board[4][5] << "   ";
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[5][3] << "  |  " << board[5][4] << "  |  " << board[5][5] << endl;
+
+	cout << "_____|_____|_____ ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____ ";
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____" << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[3][6] << "  |  " << board[3][7] << "  |  " << board[3][7] << "   ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[4][6] << "  |  " << board[4][7] << "  |  " << board[4][8] << "   ";
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[5][6] << "  |  " << board[5][7] << "  |  " << board[5][8] << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 3 || bigBoardIndex == 4){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 4 || bigBoardIndex == 5){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+
+	if (bigBoardIndex == 3 || bigBoardIndex == 6){cout << BOLDYELLOW;}
+	cout << "------------------" << BOLDBLUE;
+	if (bigBoardIndex == 3 || bigBoardIndex == 6 || bigBoardIndex == 4 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "-" << BOLDBLUE;
+	if (bigBoardIndex == 4 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "-------------------" << BOLDBLUE;
+	if (bigBoardIndex == 4 || bigBoardIndex == 7 || bigBoardIndex == 5 || bigBoardIndex == 8){cout << BOLDYELLOW;}
+	cout << "-" << BOLDBLUE;
+	if (bigBoardIndex == 5 || bigBoardIndex == 8){cout << BOLDYELLOW;}
+	cout << "------------------" << BOLDBLUE << endl;
+//
+	cout << "     |     |      ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[6][0] << "  |  " << board[6][1] << "  |  " << board[6][2] << "   ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[7][0] << "  |  " << board[7][1] << "  |  " << board[7][2] << "   ";
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[8][0] << "  |  " << board[8][1] << "  |  " << board[8][2] << endl;
+
+	cout << "_____|_____|_____ ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____ ";
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____" << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[6][3] << "  |  " << board[6][4] << "  |  " << board[6][5] << "   ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[7][3] << "  |  " << board[7][4] << "  |  " << board[7][5] << "   ";
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[8][3] << "  |  " << board[8][4] << "  |  " << board[8][5] << endl;
+
+	cout << "_____|_____|_____ ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____ ";
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << " _____|_____|_____" << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+	
+	cout << "  " << board[6][6] << "  |  " << board[6][7] << "  |  " << board[6][7] << "   ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[7][6] << "  |  " << board[7][7] << "  |  " << board[7][8] << "   ";
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;}
+	cout << "|   " << BOLDBLUE << board[8][6] << "  |  " << board[8][7] << "  |  " << board[8][8] << endl;
+
+	cout << "     |     |      ";
+	if (bigBoardIndex == 6 || bigBoardIndex == 7){cout << BOLDYELLOW;}
+	cout << "|" << BOLDBLUE << "      |     |      "; 
+	if (bigBoardIndex == 7 || bigBoardIndex == 8){cout << BOLDYELLOW;} 
+	cout << "|" << BOLDBLUE << "      |     |" << endl;
+
+	/*
 	cout << "     |     |      |      |     |      |      |     |" << endl;
 	cout << "  " << board[0][0] << "  |  " << board[0][1] << "  |  " << board[0][2] << "   |   " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << "   |   " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << endl;
 	cout << "_____|_____|_____ | _____|_____|_____ | _____|_____|_____" << endl;
@@ -233,18 +418,7 @@ void displayBoard(vector< vector<char> > board, int bigBoardIndex){
 	cout << "     |     |      |      |     |      |      |     |" << endl;	
 	cout << "  " << board[6][6] << "  |  " << board[6][7] << "  |  " << board[6][8] << "   |   " << board[7][6] << "  |  " << board[7][7] << "  |  " << board[7][8] << "   |   " << board[8][6] << "  |  " << board[8][7] << "  |  " << board[8][8] << endl;
 	cout << "     |     |      |      |     |      |      |     |     " << endl << endl;
-/*
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-_____|_____|_____ | _____|_____|_____ | _____|_____|_____
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-_____|_____|_____ | _____|_____|_____ | _____|_____|_____
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
-     |     |      |      |     |      |      |     |     
----------------------------------------------------------
-*/
+	*/
 }
 
 bool checkSpaceUltimate(vector<char> board, int index){ // returns whether the space is open or not
